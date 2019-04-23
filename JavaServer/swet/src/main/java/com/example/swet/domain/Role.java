5d652e4f71eca0021da3ef12493370b3bc349025
Titten3 <zaryvnyh1998@yandex.ru>
2019-04-24 04:37:57 +0500

    added links between database tables, connected template Freemarker

--- JavaServer/swet/src/main/java/com/example/swet/domain/Role.java
+++ JavaServer/swet/src/main/java/com/example/swet/domain/Role.java
@@ -1,5 +1,12 @@
 package com.example.swet.domain;
 
-public enum Role {
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
     USER;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 }
